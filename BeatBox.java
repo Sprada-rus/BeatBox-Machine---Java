13356cb91e0204357f298ddd5945a9f6874f5854
Sprada <sappronov@gmail.com>
2020-06-12 13:11:50 +0300

    version 2

--- BeatBox.java
+++ BeatBox.java
@@ -3,8 +3,10 @@ package bin;
 import java.awt.*;
 import javax.swing.*;
 import javax.sound.midi.*;
+import java.io.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
 import java.util.*;
 
 
@@ -53,6 +55,21 @@ public class BeatBox {
         JButton clearCheckBox = new JButton("Очистить");
         clearCheckBox.addActionListener(new MyClearCheckBoxListener());
         buttonBox.add(clearCheckBox);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Файл");
+        JMenuItem saveFile = new JMenuItem("Сохранить");
+        JMenuItem loadFile = new JMenuItem("Загрузить");
+
+        saveFile.addActionListener(new SaveFileListener());
+        loadFile.addActionListener(new LoadFileListener());
+
+        fileMenu.add(saveFile);
+        fileMenu.add(loadFile);
+        menuBar.add(fileMenu);
+
+        theFrame.setJMenuBar(menuBar);
+        theFrame.getContentPane().add(BorderLayout.NORTH, background);
 
         Box nameBox = new Box(BoxLayout.Y_AXIS);
         for (int i = 0; i < 16; i++){
@@ -127,6 +144,27 @@ public class BeatBox {
         } catch (Exception e) {e.printStackTrace();}
     }
 
+    private class SaveFileListener implements ActionListener{
+        public void actionPerformed(ActionEvent a){
+        JFileChooser fileSave = new JFileChooser();
+        fileSave.showSaveDialog(theFrame);
+        saveFile(fileSave.getSelectedFile());
+
+        sequencer.stop();
+        }
+    }
+
+    private class LoadFileListener implements ActionListener{
+        public void actionPerformed(ActionEvent actionEvent) {
+            JFileChooser fileOpen = new JFileChooser();
+            fileOpen.showOpenDialog(theFrame);
+            loadFile(fileOpen.getSelectedFile());
+
+            sequencer.stop();
+            buildTrackAndStart();
+        }
+    }
+
     public class MyStartListener implements ActionListener {
         public void actionPerformed(ActionEvent a){
             buildTrackAndStart();
@@ -160,6 +198,46 @@ public class BeatBox {
                 check.setSelected(false);
                 checkBoxList.set(i,check);
             }
+        }
+    }
+
+    public void loadFile(File nameFile){
+        boolean[] checkState = null;
+
+        try{
+            FileInputStream fileStream = new FileInputStream(new File(String.valueOf(nameFile)));
+            ObjectInputStream is = new ObjectInputStream(fileStream);
+            checkState = (boolean[]) is.readObject();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        for (int i = 0; i < 256; i++){
+            JCheckBox checkBox = (JCheckBox) checkBoxList.get(i);
+            if (checkState[i]){
+                checkBox.setSelected(true);
+            } else {
+                checkBox.setSelected(false);
+            }
+        }
+    }
+
+    public void saveFile(File nameFile){
+        boolean[] checkState = new boolean[256];
+
+        for (int i = 0; i < 256; i++){
+            JCheckBox checkBox = (JCheckBox) checkBoxList.get(i);
+            if (checkBox.isSelected()){
+                checkState[i] = true;
+            }
+        }
+
+        try {
+            FileOutputStream fileStream = new FileOutputStream(new File(String.valueOf(nameFile)));
+            ObjectOutputStream os = new ObjectOutputStream(fileStream);
+            os.writeObject(checkState);
+        } catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 
